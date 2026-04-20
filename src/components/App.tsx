eb784dd01d46788ea0c4ed7413adc3acbf8b2741
HitaBeeDev <anahita.amiri103@gmail.com>
2026-04-20 22:14:42 +0200

    refactor: replace quiz JSON data with typed module

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,7 +1,4 @@
-import { useEffect, useReducer } from 'react'
-import type { Reducer } from 'react'
-import { questions as quizQuestions } from '../data/questions'
-import type { QuizAction, QuizState } from '../types/quiz'
+import { useQuiz } from '../hooks/useQuiz'
 import FinishScreen from './FinishScreen'
 import Footer from './Footer'
 import Header from './Header'
@@ -11,105 +8,10 @@ import Question from './Question'
 import StartScreen from './StartScreen'
 import Timer from './Timer'
 
-const SECS_PER_QUESTION = 20
-
-const initialState: QuizState = {
-  questions: [],
-  index: 0,
-  answer: null,
-  secondsRemaining: null,
-  scores: {
-    logical: 0,
-    emotional: 0,
-    intrapersonal: 0,
-  },
-  status: 'ready',
-}
-
-const reducer: Reducer<QuizState, QuizAction> = (
-  state: QuizState,
-  action: QuizAction,
-): QuizState => {
-  switch (action.type) {
-    case 'dataReceived':
-      return {
-        ...state,
-        questions: action.payload,
-        status: 'ready',
-      }
-
-    case 'start':
-      return {
-        ...state,
-        status: 'active',
-        secondsRemaining: state.questions.length * SECS_PER_QUESTION,
-      }
-
-    case 'newAnswer': {
-      const selectedKey = action.payload
-      const currentQuestion = state.questions[state.index]
-
-      if (!currentQuestion) {
-        return state
-      }
-
-      const selectedOption = currentQuestion.options[selectedKey]
-
-      if (!selectedOption) {
-        return state
-      }
-
-      const updatedScores = { ...state.scores }
-      updatedScores[selectedOption.intelligence] += selectedOption.score
-
-      return {
-        ...state,
-        answer: selectedKey,
-        scores: updatedScores,
-      }
-    }
-
-    case 'restart':
-      return {
-        ...initialState,
-        questions: state.questions,
-        status: 'ready',
-      }
-
-    case 'nextQuestion':
-      return { ...state, index: state.index + 1, answer: null }
-
-    case 'finish':
-      return {
-        ...state,
-        status: 'finished',
-      }
-
-    case 'tick':
-      if (state.secondsRemaining === null) {
-        return state
-      }
-
-      return {
-        ...state,
-        secondsRemaining: state.secondsRemaining - 1,
-        status: state.secondsRemaining <= 1 ? 'finished' : state.status,
-      }
-    default:
-      throw new Error('Action Unrecognized')
-  }
-}
-
 export default function App() {
-  const [{ questions, status, index, answer, secondsRemaining, scores }, dispatch] =
-    useReducer<Reducer<QuizState, QuizAction>>(reducer, initialState)
-
-  const numQuestions = questions.length
+  const { questions, status, index, answer, secondsRemaining, scores, numQuestions, dispatch } =
+    useQuiz()
   const currentQuestion = questions[index]
-
-  useEffect(() => {
-    dispatch({ type: 'dataReceived', payload: quizQuestions })
-  }, [dispatch])
 
   return (
     <div
